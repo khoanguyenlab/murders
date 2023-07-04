60d2c160b3ebebf865a12d102d860a7dd915b589
khoanguyenlab <willykhoa.nguyen@gmail.com>
2023-07-04 13:18:19 +0200

    EdX - R - Wrangling - Sec 2 - Webscraping

--- exercise - wrangling.R
+++ exercise - wrangling.R
@@ -54,3 +54,32 @@ ta <- AwardsPlayers %>% filter(yearID == "2016" & !is.na(awardID))
 at0 <- top_names %>% select(playerID)
 at <- ta %>% select(playerID) %>% setdiff(at0)
 str(at)
+
+
+# Webscraping -  Major League Baseball payrolls
+library(rvest)
+url <- "https://web.archive.org/web/20181024132313/http://www.stevetheump.com/Payrolls.htm"
+h <- read_html(url)
+nodes <- html_nodes(h, "table")
+html_table(nodes[1:4])
+html_table(nodes[19:21])
+
+tab_1 <- html_table(nodes[10])[[1]]
+tab_1 <- tab_1 %>% slice(-1) 
+colnames(tab_1) <- c("No.","Team", "Payroll", "Average")
+tab_1 <- tab_1 %>% select(Team, Payroll, Average)
+tab_2 <- html_table(nodes[19])[[1]]
+tab_2 <- tab_2 %>% slice(-1) 
+colnames(tab_2) <- c("Team", "Payroll", "Average")
+tab_2 <- tab_2 %>% select(Team, Payroll, Average)
+tab_f <- full_join(tab_1, tab_2, by = "Team")
+nrow(tab_f)
+
+# Webscraping - Brexit referendum
+library(rvest)
+library(tidyverse)
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+h <- read_html(url)
+tab <- html_nodes(h, "table")
+tab
+html_table(tab, fill=T)
